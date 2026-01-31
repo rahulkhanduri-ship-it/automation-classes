c4e053208413f3caee16518b9e3efe73204a6467
rahul.khanduri <rahul.khanduri@timesinternet.in>
2026-01-31 09:16:29 +0530

    init commit

---
+++ src/test/java/callanotherclass.java
@@ -0,0 +1,9 @@
+public class callanotherclass {
+    public static void main (String[] args) {
+        usinghashmaps a = new usinghashmaps();
+        a.main();
+
+
+    }
+
+}
---
+++ src/test/java/loopchap1.java
@@ -0,0 +1,15 @@
+
+
+public class loopchap1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        String[] MenuName = {"TOI", "NBT", "ET", "TIL"};
+        for(String Menu : MenuName) {
+            System.out.println(Menu);
+        }
+
+    }
+
+}
