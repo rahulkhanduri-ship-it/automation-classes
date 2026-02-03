69329221b94a378b5e73f123390eaac81e700854
rahul.khanduri <rahul.khanduri@timesinternet.in>
2026-02-03 17:50:16 +0530

    init commit

---
+++ src/test/java/handlingeception.java
@@ -0,0 +1,16 @@
+public class handlingeception {
+
+        public static void main(String[] args) {
+
+            int a = 100;
+            int b = 0;
+
+            try {
+                int result = a / b;
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("FAILED: Division by zero is not allowed");
+            }
+        }
+    }
+
---
+++ src/test/java/loopchap.java
@@ -0,0 +1,16 @@
+
+
+public class loopchap {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+
+            System.out.println(i);
+        }
+        String[] MenuName = {"TOI", "NBT", "ET", "TIL"};
+        for(String Menu : MenuName) {
+            System.out.println(Menu);
+        }
+
+    }
+
+}
